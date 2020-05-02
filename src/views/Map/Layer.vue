b73619bfc76ef8f0afd1be02c4b865f48ff54839
lsq <1135595767@qq.com>
2020-05-02 17:08:48 +0800

    feat: 🎸添加 layer  组件

---
+++ src/views/Map/Layer.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <a-button style="font-size: 16px; padding: 0 6px" @click="toggleCollapsed">
+      <svg-icon svgClass="iconlayer"></svg-icon>
+    </a-button>
+    <div class="layer-container" v-if="!collapsed">
+      <p>Basemap</p>
+      <p>HotsPots</p>
+      <p>....</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import {Button} from 'ant-design-vue'
+import SvgIcon from '@/components/SvgIcon'
+
+export default {
+  data () {
+    return {
+      collapsed: false
+    }
+  },
+  components: {
+    SvgIcon,
+    AButton: Button
+  },
+  methods: {
+    toggleCollapsed () {
+      this.collapsed = !this.collapsed
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.layer-container {
+  width: 256px;
+  height: 50vh;
+  position: relative;
+  margin-top: 16px;
+  background-color: #fff;
+  padding: 16px;
+  text-align: left;
+}
+</style>
