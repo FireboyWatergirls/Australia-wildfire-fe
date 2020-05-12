5827c848874fa436ccf99ced2c481ad7a0e01a2d
WangZhiye <609683213@qq.com>
2020-05-13 00:17:41 +0800

    feat: 🎸 图层切换功能

--- src/views/Map/Layer.vue
+++ src/views/Map/Layer.vue
@@ -4,15 +4,33 @@
       <svg-icon svgClass="iconlayer"></svg-icon>
     </a-button>
     <div class="layer-container" v-if="!collapsed">
-      <p>Basemap</p>
-      <p>HotsPots</p>
-      <p>....</p>
+      <p><a-icon type="home"/> Basemap</p>
+      <a-radio style="color: white">Satellite Map</a-radio><br>
+      <a-radio style="color: white">Street View</a-radio><br>
+      <a-radio style="color: white">Dark</a-radio><br>
+      <a-radio style="color: white">Light</a-radio><br>
+      <p style="margin-top: 16px"><a-icon type="setting" theme="filled" /> HotsPots</p>
+      <a-dropdown>
+      <a-menu slot="overlay">
+        <a-menu-item key="1"> <a-icon type="user" />1st menu item </a-menu-item><br>
+        <a-menu-item key="2"> <a-icon type="user" />2nd menu item </a-menu-item><br>
+        <a-menu-item key="3"> <a-icon type="user" />3rd item </a-menu-item>
+      </a-menu>
+      <a-button style="margin-left: 16px"> Select <a-icon type="down" /> </a-button>
+    </a-dropdown><br>
+      <p style="margin-top: 16px"><a-icon type="layout" theme="filled" /> Overlays</p>
+      <a-checkbox style="color: white">3D Map
+      </a-checkbox><br>
+      <a-checkbox style="color: white">Land Classification Map
+      </a-checkbox><br>
+      <a-checkbox style="color: white">Climate Map
+      </a-checkbox>
     </div>
   </div>
 </template>
 
 <script>
-import {Button} from 'ant-design-vue'
+import {Button, Checkbox, Dropdown, Icon, Radio} from 'ant-design-vue'
 import SvgIcon from '@/components/SvgIcon'
 
 export default {
@@ -23,8 +41,13 @@ export default {
   },
   components: {
     SvgIcon,
-    AButton: Button
+    AButton: Button,
+    ACheckbox: Checkbox,
+    ADropdown: Dropdown,
+    AIcon: Icon,
+    ARadio: Radio
   },
+
   methods: {
     toggleCollapsed () {
       this.collapsed = !this.collapsed
