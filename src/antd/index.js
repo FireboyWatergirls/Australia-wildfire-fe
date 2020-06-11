eb5d7ac9431ef36728228ad361564132fbd36c78
Mosuyun <709963735@qq.com>
2020-06-11 21:25:10 +0800

    feat: 🎸 about

--- src/antd/index.js
+++ src/antd/index.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { Button, Menu, Icon, Select, Checkbox, Dropdown, Radio, Input } from 'ant-design-vue'
+import { Button, Menu, Icon, Select, Checkbox, Dropdown, Radio, Input, Carousel } from 'ant-design-vue'
 
 Vue.component(Button.name, Button)
 Vue.component(Menu.name, Menu)
@@ -12,3 +12,4 @@ Vue.component(Checkbox.name, Checkbox)
 Vue.component(Dropdown.name, Dropdown)
 Vue.component(Radio.name, Radio)
 Vue.component(Input.name, Input)
+Vue.component(Carousel.name, Carousel)
--- src/views/About/Index.vue
+++ src/views/About/Index.vue
@@ -1,17 +1,389 @@
 <template>
-  <div id="about">关于（研究目的、团队介绍）
+  <div id="about">
+    <div id="resourse">
+      <h1
+        style="font-family: 'Raleway', sans-serif;text-align:center;margin:20px;letter-spacing: 1px;font-weight:bold
+    color: #333;"
+      >WATER GIRLS AND FIRE BOY</h1>
+      <h2
+        style="font-family: 'Lato', sans-serif;text-align:center;font-weight: 300;margin-bottom:50px;
+    color: #777;"
+      >A study group works on Australia Mountain Fire.</h2>
+      <a-carousel arrows autoplay>
+        <div
+          slot="prevArrow"
+          slot-scope="props"
+          class="custom-slick-arrow"
+          style="left: 10px;zIndex: 1"
+        >
+          <a-icon type="left-circle" />
+        </div>
+        <div slot="nextArrow" slot-scope="props" class="custom-slick-arrow" style="right: 10px">
+          <a-icon type="right-circle" />
+        </div>
+        <div>
+          <img src="/static/img/fire.jpg" />
+        </div>
+        <div>
+          <img src="/static/img/git.png" />
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+      </a-carousel>
+      <h1
+        style="font-family: 'Raleway', sans-serif;text-align:center;margin:20px;letter-spacing: 1px;font-weight:7px
+    color: #333;"
+      >Resource</h1>
+      <div id="links">
+        <div style="margin-left:12px; font-size:0.875rem;line-height:1.8;color:#43869f;">
+          <p>
+            Carbon emissions reductions giving, legitimize amplify non-partisan Aga Khan. Policy dialogue assessment expert free-speech cornerstone disruptor freedom. Cesar Chavez empower.
+            Carbon emissions reductions giving, legitimize amplify non-partisan Aga Khan. Policy dialogue assessment expert free-speech cornerstone disruptor freedom. Cesar Chavez empower.
+            Carbon emissions reductions giving, legitimize amplify non-partisan Aga Khan. Policy dialogue assessment expert free-speech cornerstone disruptor freedom. Cesar Chavez empower.
+            Carbon emissions reductions giving, legitimize amplify non-partisan Aga Khan. Policy dialogue assessment expert free-speech cornerstone disruptor freedom. Cesar Chavez empower.
+          </p>
+        </div>
+      </div>
+    </div>
+    <div id="transition0"></div>
+    <div id="transition"></div>
+    <div id="background">
+      <div id="cardswrap">
+        <div id="cards">
+          <div class="card">
+            <div id="border-lsq"></div>
+            <div id="info-lsq">
+              <img class="avatarup" src="/static/avatar/lsq.jpeg" />
+              <div class="selfIntro1">
+                <h1 style="text-align:center">Lsq</h1>
+                <h2>leader</h2>
+                <p>Carbon emissions reductions giving, legitimize amplify non-partisan Aga Khan. Policy dialogue assessment expert free-speech cornerstone disruptor freedom. Cesar Chavez empower.</p>
+              </div>
+              <div class="icon1">
+                <a href="https://github.com/lsq210">
+                  <a-icon type="github" />
+                </a>
+              </div>
+              <div class="intro"></div>
+            </div>
+            <div id="border-lsq"></div>
+          </div>
+          <div class="card">
+            <div id="border-gyx"></div>
+            <div id="info-gyx">
+              <div class="icon2">
+                <a href="https://github.com/Mosuyun">
+                  <a-icon type="github" />
+                </a>
+              </div>
+              <div class="selfIntro2">
+                <p>Carbon emissions reductions giving, legitimize amplify non-partisan Aga Khan. Policy dialogue assessment expert free-speech cornerstone disruptor freedom. Cesar Chavez empower.</p>
+                <h2>group member</h2>
+                <h1 style="text-align:center">Gyx</h1>
+              </div>
+              <img class="avatardown" src="/static/avatar/gyx.jpg" />
+            </div>
+            <div id="border-gyx"></div>
+          </div>
+          <div class="card">
+            <div id="border-dmy"></div>
+            <div id="info-dmy">
+              <img class="avatarup" src="/static/avatar/dmy.jpg" />
+              <div class="selfIntro1">
+                <h1 style="text-align:center">Dmy</h1>
+                <h2>group member</h2>
+                <p>Carbon emissions reductions giving, legitimize amplify non-partisan Aga Khan. Policy dialogue assessment expert free-speech cornerstone disruptor freedom. Cesar Chavez empower.</p>
+              </div>
+              <div class="icon1">
+                <a href="https://github.com/MandyDongrs">
+                  <a-icon type="github" />
+                </a>
+              </div>
+            </div>
+            <div id="border-dmy"></div>
+          </div>
+          <div class="card">
+            <div id="border-csl"></div>
+            <div id="info-csl">
+              <div class="icon2">
+                <a href="https://github.com/Chensilu0509">
+                  <a-icon type="github" />
+                </a>
+              </div>
+              <div class="selfIntro2">
+                <p>Carbon emissions reductions giving, legitimize amplify non-partisan Aga Khan. Policy dialogue assessment expert free-speech cornerstone disruptor freedom. Cesar Chavez empower.</p>
+                <h2>group member</h2>
+                <h1 style="text-align:center">Csl</h1>
+              </div>
+              <img class="avatardown" src="/static/avatar/csl.jpg" />
+            </div>
+            <div id="border-csl"></div>
+          </div>
+          <div class="card">
+            <div id="border-wzy"></div>
+            <div id="info-wzy">
+              <img class="avatarup" src="/static/avatar/wzy.jpg" />
+              <div class="selfIntro1">
+                <h1 style="text-align:center">Wzy</h1>
+                <h2>group member</h2>
+                <p>Carbon emissions reductions giving, legitimize amplify non-partisan Aga Khan. Policy dialogue assessment expert free-speech cornerstone disruptor freedom. Cesar Chavez empower.</p>
+              </div>
+              <div class="icon1">
+                <a href="https://github.com/WangZhiyee">
+                  <a-icon type="github" />
+                </a>
+              </div>
+            </div>
+            <div id="border-wzy"></div>
+          </div>
+          <div class="card">
+            <div id="border-zyb"></div>
+            <div id="info-zyb">
+              <div class="icon2">
+                <a href="https://github.com/YBstory">
+                  <a-icon type="github" />
+                </a>
+              </div>
+              <div class="selfIntro2">
+                <p>Carbon emissions reductions giving, legitimize amplify non-partisan Aga Khan. Policy dialogue assessment expert free-speech cornerstone disruptor freedom. Cesar Chavez empower.</p>
+                <h2>group member</h2>
+                <h1 style="text-align:center">Zyb</h1>
+              </div>
+              <img class="avatardown" src="/static/avatar/zyb2.jpeg" />
+            </div>
+            <div id="border-zyb"></div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-}
+export default {}
 </script>
 
 <style lang="scss" scoped>
 #about {
   position: fixed;
-  top: 60px;
-  left: 60px;
+  top: 50px;
+  height: 100%;
+  width: 100%;
+  border: gold;
+  background-color: #f0ebe4;
+  overflow: auto;
+}
+#resource {
+  width: 100%;
+  height: 600vh;
+  //background-color: #e5e2dd;
+}
+.ant-carousel {
+  width: 50%;
+  height: 500px;
+  margin-left: 25%;
+  margin-right: 25%;
+}
+.ant-carousel >>> .slick-slide {
+  text-align: center;
+  height: 500px;
+  width: 40px;
+  line-height: 160px;
+  background: #d4d3d1;
+  overflow: hidden;
+}
+.ant-carousel >>> .custom-slick-arrow {
+  width: 25px;
+  height: 25px;
+  font-size: 25px;
+  color: #49819a;
+  background-color: rgba(31, 45, 61, 0.11);
+  opacity: 0.3;
+}
+.ant-carousel >>> .custom-slick-arrow:before {
+  display: none;
+}
+.ant-carousel >>> .custom-slick-arrow:hover {
+  opacity: 0.5;
+}
+.ant-carousel >>> .slick-slide h3 {
+  color: #fff;
+}
+#links {
+  margin-left: 20%;
+  margin-right: 20%;
+  width: 60%;
+  background-color: #efefef;
+  border-left: 3px solid #558862;
+}
+#transition0 {
+  width: 100%;
+  height: 100px;
+  background: linear-gradient(to top, #f0ebe4, #f0ebe4);
+}
+#transition {
+  width: 100%;
+  height: 100px;
+  background: linear-gradient(to top, #7bbad1ec, #f0ebe4);
+}
+#background {
+  display: flex;
+  position: relative;
+  height: 100%;
+  width: 100%;
+  //margin-left: 5%;
+  //margin-right: 5%;
+  //background: url('/static/img/bk.png');
+}
+#cardswrap {
+  display: flex;
+  position: relative;
+  height: 100%;
+  width: 100%;
+  background-color: #7bbad1ec;
+}
+#cards {
+  position: relative;
+  top: 2%;
+  left: 2.5%;
+  width: 95%;
+  height: 100%;
+  display: flex;
+}
+.card {
+  display: flex;
+  width: 20%;
+  margin: 10px;
+  height: 90%;
+}
+.avatarup {
+  position: relative;
+  width: 60%;
+  border-radius: 50%;
+  border: 3px solid #333;
+  margin-left: 20%;
+  margin-right: 20%;
+  margin-top: 3vh;
+}
+.avatardown {
+  position: relative;
+  width: 60%;
+  border-radius: 50%;
+  border: 3px solid #333;
+  margin-left: 20%;
+  margin-right: 20%;
+  margin-top: 6vh;
+}
+.icon1 {
+  font-size: 50px;
+  text-align: center;
+  z-index: inherit;
+  margin-top: 10vh;
+}
+.icon2 {
+  font-size: 50px;
+  text-align: center;
+  z-index: inherit;
+  margin-top: 10vh;
+}
+.selfIntro1 {
+  width: 90%;
+  height: 50%;
+  margin-left: 5%;
+  margin-top: 1vh;
+}
+.selfIntro2 {
+  width: 90%;
+  height: 22%;
+  margin-left: 5%;
+  margin-top: 26vh;
+}
+#border-lsq {
+  float: left;
+  height: 100%;
+  width: 7px;
+  background-color: aqua;
+  background: linear-gradient(to top, #efefef, #ff72a3);
+}
+#info-lsq {
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(to bottom, #f5a7c2e8, #efefef);
+  opacity: 0.9;
+  border-top: 7px solid #ff72a3;
+}
+#border-gyx {
+  float: left;
+  height: 100%;
+  width: 7px;
+  background-color: aqua;
+  background: linear-gradient(to bottom, #efefef, #ffcf56);
+}
+#info-gyx {
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(to top, #f3d68dee, #efefef);
+  opacity: 0.9;
+  border-bottom: 7px solid #ffcf56;
+}
+#border-dmy {
+  float: left;
+  height: 100%;
+  width: 7px;
+  background-color: aqua;
+  background: linear-gradient(to top, #efefef, #ff7272);
+}
+#info-dmy {
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(to bottom, #fd9999ee, #efefef);
+  opacity: 0.9;
+  border-top: 7px solid #ff7272;
+}
+#border-csl {
+  float: left;
+  height: 100%;
+  width: 7px;
+  background-color: aqua;
+  background: linear-gradient(to bottom, #efefef, #c978ae);
+}
+#info-csl {
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(to top, #f5aedde8, #efefef);
+  opacity: 0.9;
+  border-bottom: 7px solid #c978ae;
+}
+#border-wzy {
+  float: left;
+  height: 100%;
+  width: 7px;
+  background-color: aqua;
+  background: linear-gradient(to top, #efefef, #5888f0);
+}
+#info-wzy {
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(to bottom, #80a5f3e5, #efefef);
+  opacity: 0.9;
+  border-top: 7px solid #5888f0;
+}
+#border-zyb {
+  float: left;
+  height: 100%;
+  width: 7px;
+  background-color: aqua;
+  background: linear-gradient(to bottom, #efefef, rgb(94, 201, 108));
+}
+#info-zyb {
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(to top, rgba(141, 233, 153, 0.863), #efefef);
+  opacity: 0.9;
+  border-bottom: 7px solid rgb(94, 201, 108);
 }
 </style>
